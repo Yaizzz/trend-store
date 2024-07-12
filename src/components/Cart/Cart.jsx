d51a5fc3ef0d6d2ec1b548291905cb8e24ccc35c
Yağız Can Dinç <ycandiinc@gmail.com>
2024-07-12 10:39:05 +0300

    Cart componenti oluşturuldu

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,24 @@
+import "./Cart.css"
+const Cart = () => {
+  return (
+    <div className="offcanvas">
+        <div className="content">
+            <div className="cart-head">
+                <h2>Sepetim</h2>
+                <a href="/">X</a>
+            </div>
+            cartItems
+            <div className="total">
+                <span>Toplam Değer</span>
+                <span>10₺</span>
+            </div>
+            <div className="actions">
+                <button className="cart-order">Sipariş Ver</button>
+                <button className="cart-clear">Temizle</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Cart
