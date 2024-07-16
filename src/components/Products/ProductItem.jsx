eaab93ca8a410b99bd8563a351cbcd49662b17cc
Yağız Can Dinç <ycandiinc@gmail.com>
2024-07-16 12:30:12 +0300

    sepete ürn ekleme işlemleri yapıldı useReducer kullanıldı

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,8 +1,12 @@
+import { useContext} from "react";
+import { CartContext } from "../../context/CartProvider";
 import Card from "../UI/Card";
-import "./ProductItem.css"
+import "./ProductItem.css";
 import Rating from "./Rating";
 const ProductItem = ({ product }) => {
   const { name, description, img, price } = product;
+  const { items, addItem, totalAmount } = useContext(CartContext);
+  console.log(totalAmount);
   return (
     <Card>
       <img src={img} alt="" />
@@ -12,7 +16,7 @@ const ProductItem = ({ product }) => {
         <Rating />
         <span className="price">{price}₺</span>
       </div>
-      <button className="add-to-cart">Sepete Ekle</button>
+      <button className="add-to-cart" onClick={()=> addItem(product)}>Sepete Ekle</button>
     </Card>
   );
 };
--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,16 +1,49 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const CartContext = createContext();
 
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      let updatedItems = [...state.items];
+      updatedItems = [...state.items, action.item];
+      return {
+        items: updatedItems,
+        totalAmount: state.totalAmount + action.item.price * action.item.amount
+      };
+    case "REMOVE":
+      return state;
+    case "CLEAR":
+      return state;
+    default:
+      return state;
+  }
+};
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
 const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
   const cartContext = {
-    items: [],
-    totalAmount: 0,
-    addItem: () => {},
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: (item) => {
+      dispatchCartAction({ type: "ADD", item });
+    },
     removeItem: () => {},
     clearItem: () => {},
   };
-  return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
